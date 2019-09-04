e461c17037b444997533ec323bccf0ade30a590e
Микола Бондарчук <bondarchuk_ak13@nuwm.edu.ua>
2019-09-04 23:49:32 +0300

    first commit

---
+++ liveChat/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using liveChat.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace liveChat.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
